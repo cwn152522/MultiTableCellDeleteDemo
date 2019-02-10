59c0f296a068ae388480ca61004f1140be0a2f94
chenweinan <1014949353@qq.com>
2019-02-10 16:27:08 +0800

    Initial Commit

---
+++ MultiTableCellDeleteDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultiTableCellDeleteDemo
+//
+//  Created by mac on 2019/2/10.
+//  Copyright © 2019年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MultiTableCellDeleteDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  MultiTableCellDeleteDemo
+//
+//  Created by mac on 2019/2/10.
+//  Copyright © 2019年 mac. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
